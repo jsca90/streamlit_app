6776501ea3c22d929fcb1e89927510645bc575d4
jsca90 <mbc4805@gmail.com>
2022-06-19 00:12:58 +0900

    local modify

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,18 @@
 
 import streamlit as st
-
 import pandas as pd
+import numpy as np
+
 
 st.write("""
 # My first App Hello world!*
 """)
+
+
+
+
+df = pd.DataFrame(
+     np.random.randn(1000, 2) / [50, 50] + [36.819044, 127.114893],
+     columns=['lat', 'lon'])
+
+st.map(df)
