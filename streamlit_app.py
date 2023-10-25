c711f07f46bc1781f2f56b380d6cf4105813dbf0
jsca90 <78601997+jsca90@users.noreply.github.com>
2023-10-25 15:35:18 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,17 @@
 import streamlit as st
 
-col1, col2 = st.columns(2)
-st.date_input('Date input')
-st.date_input('Date input')
+
+
+# Three columns with different widths
+col1, col2, col3 = st.columns([3,1,1])
+# col1 is wider
+
+# Using 'with' notation:
+with col1:
+  st.date_input('Date input')
+
+with col2:
+  st.date_input('Date input')
+
+with col3:
+  st.text('g')
