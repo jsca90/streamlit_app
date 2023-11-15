022cbee95bebf3094cbb58ddae99a5f5096cd3eb
SCA J <78601997+jsca90@users.noreply.github.com>
2023-11-15 22:27:33 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,16 @@
 import streamlit as st
 
+st.title('CodeSandbox')
 
 start = st.date_input(label='Start: ',
-              value=None,
-              key='#start',
-              help="The start date time",
-                      on_change=lambda : None)
+                      value=None,
+                      key='#start',
+                      help="The start date time",
+                      on_change=lambda: None)
 
 end = st.date_input(label='End: ',
-              value=None,
-              key='#end',
-              help="The end date time",
-                    on_change=lambda : None)
+                    value=None,
+                    key='#end',
+                    help="The end date time",
+                    on_change=lambda: None)
 st.write('Start: ', start, "End: ", end)
